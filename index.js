f4bdc2d97cdd7e61ade72658f1c1607ff7f033a8
Stephen N. Karagan <stevekaragan@gmail.com>
2020-11-25 10:22:41 -0500

    comments tidy code

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
 'use strict';
-
-//figure out how states are entered
-//clean up code
-//neaten up CSS
-//look at address again briefly
-//think bout shorter coding, and better iteration
-
-
+// api key from site, search URL is endpoint
 const apiKey = 'xwhnDftgxO4RpDZJIWPIH9XItqx0AiAHDNbn4GlF'; 
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
+//this is for the stretch goal.  I used physical address.
 function createAddress(parkObj) {
-    for (let j = 0; j < parkObj.addresses.length; j++) {
-        if (parkObj.addresses[j].type='physical') {         
-        let addressObj = parkObj.addresses[j]
-        let address = `${addressObj.line1} ${addressObj.line2} ${addressObj.line3} | ${addressObj.city} | ${addressObj.stateCode} | ${addressObj.postalCode}`
-        return address
-        };         
-    };
+  for (let j = 0; j < parkObj.addresses.length; j++) {
+    if (parkObj.addresses[j].type='physical') {         
+    let addressObj = parkObj.addresses[j]
+    let address = `${addressObj.line1} ${addressObj.line2} ${addressObj.line3} | 
+    ${addressObj.city} | ${addressObj.stateCode} | ${addressObj.postalCode}`
+    return address
+    };         
+  };
+};
+//empty out previous searches & diplay current results
+function displayResults(responseJson) {
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.data.length; i++){
+    let address = createAddress(responseJson.data[i])
+    let park = responseJson.data[i]
+    $('#results-list').append(
+      `<li><h3>${park.fullName}</h3>
+      <p>${park.description}</p>
+      <a href='${park.url}' target="_blank">Website</a>
+      <p>${address}</p>
+      </li>`
+    )};
+  $('#results').removeClass('hidden');
 };
 
-function displayResults(responseJson) {
-    $('#results-list').empty();
-    // iterate through the items array
-    for (let i = 0; i < responseJson.data.length; i++){
-        let address = createAddress(responseJson.data[i])
-        $('#results-list').append(
-            `<li><h3>${responseJson.data[i].fullName}</h3>
-            <p>${responseJson.data[i].description}</p>
-            <a href='${responseJson.data[i].url}' target="_blank">Website</a>
-            <p>${address}</p>
-            </li>`
-      )};
-    //display the results section  
-    $('#results').removeClass('hidden');
-  };
-
+//add parameters to URL, fetch data, error handling
 function getParks(states, maxResults=10) {
-  const url = searchURL + '?' + `stateCode=${states}&limit=${maxResults}&api_key=${apiKey}`;
-  console.log(url)
+  const url = searchURL + '?' + `stateCode=${states}&limit=${maxResults}&api_key=${apiKey}`
 
   fetch(url)
     .then(response => {
@@ -53,8 +47,8 @@ function getParks(states, maxResults=10) {
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
-}
-
+};
+//event listener to know when search initiated, executes search
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
@@ -62,6 +56,6 @@ function watchForm() {
     const maxResults = $('#js-max-results').val();
     getParks(states, maxResults);
   });
-}
+};
 
 $(watchForm);
